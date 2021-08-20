bbf56a2450634b852aa7d6f47a136f803267c9d1
artem-ca <capuchin@live.ru>
2021-08-20 23:47:31 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,104 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Info from './Components/Info'
+import Weather from './Components/Weather'
+import Form from './Components/Form'
+
+import './App.css'
+
+const API_KEY = '6f77c624e27be5f8a02f8d28bc9a8634'
+
+class App extends React.Component {
+  state = {
+    temp: undefined,
+    city: undefined,
+    country: undefined,
+    pressure: undefined,
+    // sunrise: undefined,
+    sunset: undefined,
+    error: undefined,
+  }
+
+  gettingWeather = async (e) => {
+    e.preventDefault()
+    var city = e.target.elements.city.value
+
+    if (city) {
+      const api_url = await fetch(
+        `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`
+      )
+      const data = await api_url.json()
+
+      // var sunrise_data = data.sys.sunrise
+      var sunset_data = data.sys.sunset
+
+      // var date_sunrise = new Date()
+      var date_sunset = new Date()
+
+      // date_sunrise.setTime(sunrise_data)
+      date_sunset.setTime(sunset_data)
+
+      // var sunrise =
+      //   sunrise.getHours() +
+      //   ':' +
+      //   sunrise.getMinutes() +
+      //   ':' +
+      //   sunrise.getSeconds()
+
+      var sunset =
+        date_sunset.getHours() +
+        ':' +
+        date_sunset.getMinutes() +
+        ':' +
+        date_sunset.getSeconds()
+
+      this.setState({
+        temp: data.main.temp,
+        city: data.name,
+        country: data.sys.country,
+        pressure: data.main.pressure,
+        sunset: sunset,
+        error: undefined,
+      })
+    } else {
+      this.setState({
+        temp: undefined,
+        city: undefined,
+        country: undefined,
+        pressure: undefined,
+        // sunrise: undefined,
+        sunset: undefined,
+        error: 'Ошиб0чка!',
+      })
+    }
+  }
+
+  render() {
+    return (
+      <div className='wrapper'>
+        <div className='main'>
+          <div className='container'>
+            <div className='row'>
+              <div className='col-sm-5 info'>
+                <Info />
+              </div>
+              <div className='col-sm-7 form'>
+                <Form gettingWeather={this.gettingWeather} />
+                <Weather
+                  temp={this.state.temp}
+                  city={this.state.city}
+                  country={this.state.country}
+                  pressure={this.state.pressure}
+                  // sunrise={this.state.sunrise}
+                  sunset={this.state.sunset}
+                  error={this.state.error}
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
 }
-
-export default App;
+export default App
---
+++ src/Components/Info.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Info = () => (
+  <div>
+    <h2>Погодное приложенние!</h2>
+    <p>Узнай погоду в любом городе</p>
+  </div>
+)
+
+export default Info
